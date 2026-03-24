0a89cfbb599ab0a55f4642064d47c7d301a66288
Ferrum-Language <noreply@ferrum-lang.org>
2026-03-23 21:19:21 -0300

    chore: update SHA256s for v0.3.0 CI-built binaries

--- Formula/ferrumc.rb
+++ Formula/ferrumc.rb
@@ -7,27 +7,24 @@ class Ferrumc < Formula
   on_macos do
     on_arm do
       url "https://github.com/Ferrum-Language/Ferrum/releases/download/v0.3.0/ferrumc-v0.3.0-macos-arm64.tar.gz"
-      # sha256 will be updated when macOS CI produces the binary
-      sha256 :no_check
+      sha256 "dd299666b2f859ff1f07fc7494d8dffe4b9f568e132168531ab5636b9011f520"
     end
     on_intel do
       url "https://github.com/Ferrum-Language/Ferrum/releases/download/v0.3.0/ferrumc-v0.3.0-macos-x86_64.tar.gz"
-      sha256 :no_check
+      sha256 "bc22feaf923829d4c8f18ff2b69ab4ed369b52b2d84244301167be4801c5082f"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/Ferrum-Language/Ferrum/releases/download/v0.3.0/ferrumc-v0.3.0-linux-aarch64.tar.gz"
-      sha256 "9284560230b179cfa7d78dea6af1d1d3abd5379279e7aa8ecb2be8262e236fca"
+      sha256 "4324815376c13b8038705864805c4d5b0e44910b6cc52c3111ec311afcc4cf9c"
     end
     on_intel do
       url "https://github.com/Ferrum-Language/Ferrum/releases/download/v0.3.0/ferrumc-v0.3.0-linux-x86_64.tar.gz"
-      sha256 "1cea7ef7eb58426fe8ec5942d16bee985e218a5997028fc05f137a0c31dd405b"
+      sha256 "2735cf702ff9c3f3bf9ad95140bde211f3e642590e9222800f81969727ea6029"
     end
   end
-
-  depends_on "llvm@18"
 
   def install
     bin.install "ferrumc"
